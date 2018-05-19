502051e063a465a87c05e1561e6b7ec3f8923d20
Renato Suga <renato@localhost.localdomain>
2018-05-19 14:03:38 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from ClassLibrary.Department import Department 
+import moduletest
+
+print (moduletest.ageofqueen)
+cfcpiano = moduletest.Piano()
+cfcpiano.printdetails()
+
+depa = Department("aaa")
+
+print(depa)
