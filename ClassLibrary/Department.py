a9d138660bdd48022f85f592ed49f6a3a4fc1e3a
Renato Suga <renato@localhost.localdomain>
2018-05-19 15:58:55 -0400

    adding employees to dept list

--- ClassLibrary/Department.py
+++ ClassLibrary/Department.py
@@ -1,9 +1,19 @@
 class Department:
+    # on garde chaque departement ajouté dans la liste
+    __lstNameDept = []
+    
     def __init__(self, Name, ListEmployee=[]):
-        self.Name = Name
+        self.Name = Name.upper()
         self.ListEmployee = ListEmployee
-    def AddEmployee(self, employee):        
-        self.ListEmployee.append(employee)
+        self.__lstNameDept.append(self.Name)
+    
+    def AddEmployee(self, employee):
+        #ajoute seulement si le departement existe
+        if employee.Department.upper() in self.__lstNameDept:
+            self.ListEmployee.append(employee)
+        else:
+            print("Error: deparment %s does not exist! ", employee.Department)
         # print("Employee: [" + employee.LastName.capitalize() +" "+ employee.FirstName+"]  added to the list" )
+    
     def PrintName(self):
         print(self.Name)
--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from ClassLibrary.Employee import Employee
 depa = Department("aaa")
 depa.PrintName()
 
-empl = Employee("First", "Last", "1000", "IT")
+depIt = Department("It")
 
-depa.AddEmployee(empl)
+empl1 = Employee("First", "aaa", "10", "aAa")
+empl2 = Employee("First", "Last", "1000", "IT")
+empl3 = Employee("Fulano", "bla bla","2344","xyz")
+
+depa.AddEmployee(empl1)
+depa.AddEmployee(empl2)
+depa.AddEmployee(empl3)
 
 x=1
