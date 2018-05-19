8596c91dbb3f8b934f58ec1e498713d57cbe8c0d
Renato Suga <renato@localhost.localdomain>
2018-05-19 14:22:00 -0400

    added new class Employee

---
+++ ClassLibrary/Employee.py
@@ -0,0 +1,6 @@
+class Employee:
+    def __init__(self, FirstName, LastName, Salary, Department):
+        self.FirstName = FirstName
+        self.LastName = LastName
+        self.Salary = Salary
+        self.Department = Department
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from ClassLibrary.Department import Department 
+from ClassLibrary.Employee import Employee
 
 depa = Department("aaa")
-
 depa.PrintName()
+
+empl = Employee("First", "Last", "1000", "IT")
+
+x=1
